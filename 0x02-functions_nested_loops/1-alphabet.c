4ca4527c7d3687a0a43b3284eb663aa3367c55dc
Nourel29 <nourelhoudam95@gmail.com>
2023-03-21 12:40:47 -0700

    task2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
-#include "main.h"
+#include <stdio.h>
 
 void print_alphabet(void);
 
-int main(void)	
+int main(void)
 {
     print_alphabet();
     return 0;
 }
 
-void print_alphabet(void) 
+void print_alphabet(void)
 {
     char letter = 'a';
-    while (letter <= 'z') 
+    while (letter <= 'z')
     {
         putchar(letter);
         letter++;
